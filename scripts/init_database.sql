748735610bbf3433e5a4dc19f75d00796dbe75b3
Vignesh Nadar <vigneshsn23@outlook.com>
2025-03-30 17:05:20 +0530

    Added the sql script file

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+
+-- Creating and using the DataWarehouse database
+
+create database DataWarehouse;
+go
+
+use DataWarehouse;
+go
+
+-- Creating schemas
+
+create schema bronze;
+go
+
+create schema silver;
+go
+
+create schema gold;
